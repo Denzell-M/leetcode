fa8ae2af1f49307d1a9af1aa30f0b0b3f29b56d0
Denzell <58393180+Denzell-M@users.noreply.github.com>
2025-02-01 12:38:18 -0500

    Day 5 of challenges

--- 2635-transform-el-inArray.ts
+++ 2635-transform-el-inArray.ts
@@ -38,5 +38,9 @@ fn returns an integer.
 */
 
 function map(arr: number[], fn: (n: number, i: number) => number): number[] {
-    
+    const result: number[] = [];
+    for (let i = 0; i < arr.length; i++) {
+        result.push(fn(arr[i], i));
+    }
+    return result;
 };
